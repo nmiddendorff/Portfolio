e396fbccb8cbb295a72d275febd458c24ef16ef7
nmiddendorff <nmiddendorff@hy-vee.com>
2020-11-15 13:08:20 -0600

    Added _app and _document from superprops - actually

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import { Modal } from '@redq/reuse-modal';
+import '@redq/reuse-modal/es/index.css';
+// import 'rc-tabs/assets/index.css';
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <Modal />
+      <Component {...pageProps} />
+    </Fragment>
+  );
+};
+
+export default App
