dbb2f60940a4e0a1a805f39a7269401a54380ea2
nmiddendorff <nmiddendorff@hy-vee.com>
2020-11-18 23:28:48 -0600

    Added favicon and Heap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { Modal } from '@redq/reuse-modal';
 // import '@redq/reuse-modal/es/index.css';
 // import 'rc-tabs/assets/index.css';
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    heap?.identify('unique_identifier');
+    heap?.addUserProperties({'VisitorType': 'Web'});
+  }, [])
+
   return (
     <Fragment>
       <Modal />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-// import FavIcon from 'common/src/assets/image/favicon.png';
+import FavIcon from '../../common/src/assets/image/Middweb_fav.png';
 
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx) {
@@ -31,7 +31,16 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <link rel="shortcut icon" type="image/x-icon" href={FavIcon} /> */}
+          <link rel="shortcut icon" type="image/x-icon" href={FavIcon} />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+                heap.load("1207938959");
+                `
+            }}
+          />
         </Head>
         <body>
           <Main />
